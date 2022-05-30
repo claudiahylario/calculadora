1153a31c54b45673268c3f5c49dad9b71cfdcd49
claudiahylario <claudiahylario@gmail.com>
2022-05-30 18:18:18 -0300

    primeiro commit

--- src/main/java/calculadora/Principal.java
+++ src/main/java/calculadora/Principal.java
@@ -1,29 +1,28 @@
 package calculadora;
 import javax.swing.JOptionPane;
-import calculadora.Calculadora;
 
 public class Principal {
     public static void main(String[] args) {
         String opcao = "";
         Calculadora calculadora = new Calculadora();
         while (!opcao.equals("9")) {
-            opcao = JOptionPane.showInputDialog("1Leitura\n²-Soma\n3 Subtração"
-                    + "\n4-Produto\n5-Divisão\n9-Sair");
+            opcao = JOptionPane.showInputDialog("1 - Leitura\n2 - Soma \n3 - Subtração"
+                    + "\n4 - Produto\n5 - Divisão\n9 - Sair");
             switch (Integer.parseInt(opcao)) {
-                case1:
+                case 1:
                     calculadora.setValorA(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor A")));
                     calculadora.setValorB(Double.parseDouble(JOptionPane.showInputDialog("Digite o valor B")));
                     break;
-                case2:
+                case 2:
                     JOptionPane.showMessageDialog(null, "Soma:" + calculadora.getAdicao());
                     break;
-                case3:
+                case 3:
                     JOptionPane.showMessageDialog(null, "Subtração:" + calculadora.getSubtracao());
                     break;
-                case4:
+                case 4:
                     JOptionPane.showMessageDialog(null, "Produto:" + calculadora.getProduto());
                     break;
-                case5:
+                case 5:
                     JOptionPane.showMessageDialog(null, "Divisão:" + calculadora.getDivisao());
                     break;
             }
